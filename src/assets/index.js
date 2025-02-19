72c9fdf11beb78ea2f6f6110ef153dcb487dc9ad
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-02-19 22:02:39 +0800

    Remove some pictures because too many picture causing lag. Then add 1 picture to finallize the picture route

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,15 +4,11 @@ import bday2 from './bday2.jpg'
 
 import bday3 from './bday3.jpg'
 
-import bday6 from './bday6.jpg'
-
-import bday7 from './bday7.jpg'
-
 import bday8 from './bday8.jpg'
 
 import picture2 from './picture2.jpg'
 
-import picture3 from './picture3.jpg'
+import us from './us.jpg'
 
 import confetti from './confetti.gif'
 
@@ -24,11 +20,9 @@ export {
     bg,
     bday2,
     bday3,
-    bday6,
-    bday7,
     bday8,
     picture2,
-    picture3,
+    us,
     confetti,
     CakeSVG
 }
